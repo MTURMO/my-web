e6a15d22b0b1c5ab7ec52ac0e4c617811dd43750
marcturmo <marc.turmo@students.salle.url.edu>
2025-10-07 14:34:47 +0200

    a

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-
 const SUPABASE_URL = "https://blefcapivbvswutesdsc.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsZWZjYXBpdmJ2c3d1dGVzZHNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3NTczNTksImV4cCI6MjA3NTMzMzM1OX0.K0RlQYKfY3lfFa3Noi5Og7QBRSfhT7S3u7SJrp_nJxk";
 
