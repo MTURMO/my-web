0d21c4e9a834b998f0b754583ecee6009ba8b310
marcturmo <marc.turmo@students.salle.url.edu>
2025-10-07 13:48:37 +0200

    va

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
 <script type="module">
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://blefcapivbvswutesdsc.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+  const supabaseUrl = 'https://blefcapivbvswutesdsc.supabase.co'
+  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsZWZjYXBpdmJ2c3d1dGVzZHNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3NTczNTksImV4cCI6MjA3NTMzMzM1OX0.K0RlQYKfY3lfFa3Noi5Og7QBRSfhT7S3u7SJrp_nJxk'
+  const supabase = supabase.createClient(supabaseUrl, supabaseKey)
 </script>
