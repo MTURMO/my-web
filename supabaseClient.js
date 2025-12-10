5d68fd407f4c88591b15864f25e0e66e7cc2bc95
marcturmo <marc.turmo@students.salle.url.edu>
2025-12-10 17:09:07 +0100

    va

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,12 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.7/dist/supabase.min.js"></script>
-
-<script>
-  const SUPABASE_URL = "https://blefcapivbvswutesdsc.supabase.co";
-  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsZWZjYXBpdmJ2c3d1dGVzZHNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3NTczNTksImV4cCI6MjA3NTMzMzM1OX0.K0RlQYKfY3lfFa3Noi5Og7QBRSfhT7S3u7SJrp_nJxk";
-
-  window.supabase = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-  );
-</script>
+export const supabase = createClient(
+  "https://blefcapivbvswutesdsc.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsZWZjYXBpdmJ2c3d1dGVzZHNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3NTczNTksImV4cCI6MjA3NTMzMzM1OX0.K0RlQYKfY3lfFa3Noi5Og7QBRSfhT7S3u7SJrp_nJxk"
+);
